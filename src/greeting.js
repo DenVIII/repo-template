1b986d03eed0bce2d610bdb34b23c23618d98530
DenVIII <Denis226@gmail.com>
2024-09-01 19:41:00 +0300

    Clean up

--- src/greeting.js
+++
@@ -1,9 +0,0 @@
-// greeting.js
-import backgroundImg from "./background.JPG"
-
-const image = document.createElement("img")
-image.src = backgroundImg
-
-document.body.appendChild(image)
-
-export const greeting = "Hello, Odinite!"
